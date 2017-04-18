d1660704f65b0a9eb4d62aa73f9312f310f0ba54
Emily Mattison <egmatti@gmail.com>
2017-04-18 12:55:23 -0400

    Continued working on coffee app project page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-// var hello = "Hello World";
-//
-// document.write(hello);
-// console.log(hello);
-
 // $(document).ready(function(){
 //   // jQuery code
 // });
+
+
+// PARALLAX
+// function InitParalax() {
+//     var paralaxDom = document.getElementsByClassName("parallaxable")[0];
+//     window.addEventListener("GlobalScroll", function(e) {
+//         if (window.getComputedStyle(paralaxDom).position == "absolute") {
+//             var perc = Math.min(e.target.scrollY / (verge.viewportH()), 1);
+//             paralaxDom.style.transform = "translateY(" + (-perc*300) + "px)";
+//         }
+//     });
+// }
+//
+//
+//
+// document.addEventListener("DOMContentLoaded", function(e) {
+//     InitParalax();

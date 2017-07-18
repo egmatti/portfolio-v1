37e51ff451116eebb438f2a8a125f2b0a25e65ca
Emily Mattison <egmatti@gmail.com>
2017-07-18 17:14:32 -0400

    Added JavaScript so that the height of the directory figures would change based on the page width

--- js/script.js
+++ js/script.js
@@ -31,3 +31,18 @@ $(".button").click(function() {
   $(this).css({"background": "#1fd4f9", "border": "2px solid #1fd4f9"});
   $(this).find("span").css("color", "#fff");
 });
+
+
+// DIRECTORY ROW FIGURE HEIGHT
+
+$(document).ready(function(){
+  var secondRowFigureWidth = $(".directory__figure--second-row .directory__image-container").outerWidth();
+  var secondRowFigureHeight = secondRowFigureWidth / 2;
+  $(".directory__figure--second-row .directory__image-container").css({"height": secondRowFigureHeight});
+});
+
+// $(document).ready(function(){
+//   var thirdRowFigureWidth = $(".directory__figure--third-row .directory__image-container").outerWidth();
+//   var thirdRowFigureHeight = thirdRowFigureWidth / 2;
+//   $(".directory__figure--third-row .directory__image-container").css({"height": thirdRowFigureHeight});
+// });
--- js/the-latest.js
+++ js/the-latest.js
@@ -1,8 +1,8 @@
 // BLOG NAV HEIGHT
 
 $(document).ready(function(){
-  var firstFigureHeight = $("#the-latest-page .directory__figure--first").outerHeight();
-  var blogNavContainerHeight = firstFigureHeight - 100 - 50;
+  var firstRowFigureHeight = $("#the-latest-page .directory__figure--first-row").outerHeight();
+  var blogNavContainerHeight = firstRowFigureHeight - 100 - 50;
   var blogNavHeight = blogNavContainerHeight - 75;
   $("#the-latest-page .blog-nav-container").css({"height": blogNavContainerHeight});
   $("#the-latest-page .blog-nav").css({"height": blogNavHeight});

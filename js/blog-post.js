cffa69aafbec0296bc07406c22d2b9456d8fea7e
Emily Mattison <egmatti@gmail.com>
2017-07-18 12:35:01 -0400

    Made share figure on Blog Post page fixed on scroll

---
+++ js/blog-post.js
@@ -0,0 +1,21 @@
+// FIXED SHARE FIGURE ON SCROLL
+
+// Get the headers position from the top of the page, plus its own height
+var startY = 445;
+
+$(window).scroll(function(){
+    checkY();
+});
+
+function checkY(){
+    if( $(window).scrollTop() > startY ){
+        $("#blog-post-page figure").css({"margin-left": "8.25%", "position": "fixed", "transform": "translate(25px, -450px)"})
+        $("#blog-post-page .blog-post-container").css({"left": "190px"})
+    } else {
+      $("#blog-post-page figure").css({"margin-left": "0%", "position": "relative", "transform": "translate(0px, 0px)"})
+      $("#blog-post-page .blog-post-container").css({"left": "60px"})
+    }
+}
+
+// Do this on load just in case the user starts half way down the page
+checkY();

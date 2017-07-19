2cc946be0318c2849ffc1ae82b711f9712c030dd
Emily Mattison <egmatti@gmail.com>
2017-07-19 11:43:28 -0400

    Added styling to handle the directory row transforms in CSS instead of JavaScript

--- js/the-latest.js
+++ js/the-latest.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
   $("#the-latest-page .blog-nav").css({"height": blogNavHeight});
 
   // var secondRowTranslate = blogNavHeight + 75 + 50;
-  var secondRowTranslate = 50;
-  $("#the-latest-page .directory__row--second").css({"transform": "translateY(-" + secondRowTranslate + "px)"});
-  $("#the-latest-page .directory__row--other").css({"transform": "translateY(-" + secondRowTranslate + "px)"});
-  $("#the-latest-page .directory__row--last").css({"transform": "translateY(-" + secondRowTranslate + "px)", "margin-bottom": "-" + secondRowTranslate + "px"});
+  // var secondRowTranslate = 50;
+  // $("#the-latest-page .directory__row--second").css({"transform": "translateY(-" + secondRowTranslate + "px)"});
+  // $("#the-latest-page .directory__row--other").css({"transform": "translateY(-" + secondRowTranslate + "px)"});
+  // $("#the-latest-page .directory__row--last").css({"transform": "translateY(-" + secondRowTranslate + "px)", "margin-bottom": "-" + secondRowTranslate + "px"});
 });

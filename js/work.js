162d02a49af740954eefd7349c5f6815c13db0bc
Emily Mattison <egmatti@gmail.com>
2017-07-19 13:56:18 -0400

    Changed the images on the Work page and added minor styling

---
+++ js/work.js
@@ -0,0 +1,15 @@
+// BLOG NAV HEIGHT AND DIRECTORY ROW POSITIONING
+
+$(document).ready(function(){
+  var firstRowFigureHeight = $("#work-page .directory__figure--first-row").outerHeight();
+  var blogNavContainerHeight = firstRowFigureHeight - 100 - 50;
+  var blogNavHeight = blogNavContainerHeight - 75;
+  $("#work-page .blog-nav-container").css({"height": blogNavContainerHeight});
+  $("#work-page .blog-nav").css({"height": blogNavHeight});
+
+  // var secondRowTranslate = blogNavHeight + 75 + 50;
+  // var secondRowTranslate = 50;
+  // $("#the-latest-page .directory__row--second").css({"transform": "translateY(-" + secondRowTranslate + "px)"});
+  // $("#the-latest-page .directory__row--other").css({"transform": "translateY(-" + secondRowTranslate + "px)"});
+  // $("#the-latest-page .directory__row--last").css({"transform": "translateY(-" + secondRowTranslate + "px)", "margin-bottom": "-" + secondRowTranslate + "px"});
+});

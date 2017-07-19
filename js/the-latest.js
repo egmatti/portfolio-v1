1d7a2e090127d158cafc1aa74a8d6f11781f52b0
Emily Mattison <egmatti@gmail.com>
2017-07-19 11:33:16 -0400

    Added JavaScript to make the directory pages have equal margins

--- js/the-latest.js
+++ js/the-latest.js
@@ -1,4 +1,4 @@
-// BLOG NAV HEIGHT
+// BLOG NAV HEIGHT AND DIRECTORY ROW POSITIONING
 
 $(document).ready(function(){
   var firstRowFigureHeight = $("#the-latest-page .directory__figure--first-row").outerHeight();
@@ -6,4 +6,10 @@ $(document).ready(function(){
   var blogNavHeight = blogNavContainerHeight - 75;
   $("#the-latest-page .blog-nav-container").css({"height": blogNavContainerHeight});
   $("#the-latest-page .blog-nav").css({"height": blogNavHeight});
+
+  // var secondRowTranslate = blogNavHeight + 75 + 50;
+  var secondRowTranslate = 50;
+  $("#the-latest-page .directory__row--second").css({"transform": "translateY(-" + secondRowTranslate + "px)"});
+  $("#the-latest-page .directory__row--other").css({"transform": "translateY(-" + secondRowTranslate + "px)"});
+  $("#the-latest-page .directory__row--last").css({"transform": "translateY(-" + secondRowTranslate + "px)", "margin-bottom": "-" + secondRowTranslate + "px"});
 });

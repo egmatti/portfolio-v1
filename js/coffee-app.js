cb423df07653112d78afd40708bf582926b081ac
Emily Mattison <egmatti@gmail.com>
2017-07-20 18:36:51 -0400

    Started migrating content and styles from portfolio-v1's Coffee App page to portfolio-v2's

--- js/coffee-app.js
+++ js/coffee-app.js
@@ -1,14 +1,17 @@
-// PARALLAX
-//
-// function InitParalax() {
-//     var paralaxDom = document.getElementsByClassName("parallaxable")[0];
-//     window.addEventListener("GlobalScroll", function(e) {
-//         if (window.getComputedStyle(paralaxDom).position == "absolute") {
-//             var perc = Math.min(e.target.scrollY / (verge.viewportH()), 1);
-//             paralaxDom.style.transform = "translateY(" + (-perc*300) + "px)";
-//         }
-//     });
-// }
-//
-// document.addEventListener("DOMContentLoaded", function(e) {
-//     InitParalax();
+// MOCKUPS SLIDER
+
+$(".synced-slider-for").slick({
+ arrows: false,
+ asNavFor: ".synced-slider-nav",
+ fade: true,
+ slidesToScroll: 1,
+ slidesToShow: 1
+});
+
+$(".synced-slider-nav").slick({
+  arrows: true,
+  asNavFor: ".synced-slider-for",
+  infinite: true,
+  slidesToScroll: 1,
+  slidesToShow: 3
+});

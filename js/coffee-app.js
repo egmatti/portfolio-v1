d277f17dfe8140bf5b34b5d2580a1a92a66c78c4
Emily Mattison <egmatti@gmail.com>
2017-07-25 12:17:19 -0400

    Added content and styled mockups and protoype sections on Coffee App page

--- js/coffee-app.js
+++ js/coffee-app.js
@@ -15,3 +15,11 @@ $(".synced-slider-nav").slick({
   slidesToScroll: 1,
   slidesToShow: 3
 });
+
+
+// PROTOTYPE SECTION HEIGHT
+
+$(document).ready(function(){
+  var prototypeImageContainerHeight = $("#coffee-app-page .prototype-image-container").outerHeight() + 200;
+  $("#coffee-app-page .prototype-section").css({"height": prototypeImageContainerHeight});
+});

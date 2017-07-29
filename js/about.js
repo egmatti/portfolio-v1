575438ad32c4ce5c104b18ade0704edc3c4c3d2d
Emily Mattison <egmatti@gmail.com>
2017-07-29 13:26:05 -0400

    Added media queries for Work page

--- js/about.js
+++ js/about.js
@@ -63,7 +63,7 @@ checkY();
 // checkContactSectionY();
 
 
-// CONTACT SECTION SCROLL REVEAL
+// SCROLL REVEAL
 
 window.sr = ScrollReveal({distance: "30px", duration: 500, scale: 0});
 sr.reveal('.contact-section .left-column', {delay: 250});

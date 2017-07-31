a6badaf56327d7b7530168b85b1d172916b794b2
Emily Mattison <egmatti@gmail.com>
2017-07-30 20:54:01 -0400

    Added tablet media queries for About page

--- js/about.js
+++ js/about.js
@@ -5,6 +5,7 @@
 // OVERVIEW SECTION FADE IN ON SCROLL
 
 const mediaQuerySmall = window.matchMedia( "(max-width: 767px)" );
+const mediaQueryMedium = window.matchMedia( "(max-width: 1025px)" );
 
 // Get the position from the top of the page
 var startY = 80;
@@ -23,8 +24,12 @@ function checkY(){
       // window width is less than 767px
       $overviewLeft.velocity({translateY: "0px"});
       $overviewRight.velocity({translateY: "0px"});
+    } else if (mediaQueryMedium.matches) {
+      // window width is less than 1025px
+      $overviewLeft.velocity({translateY: "0px"});
+      $overviewRight.velocity({translateY: "0px"});
     } else {
-      // window width is at least 767px
+      // window width is at least 1025px
       $overviewLeft.velocity({opacity: 1, translateY: "-30px"}, {visibility: "visible", duration: 500, easing: "cubic-bezier(0.6, 0.2, 0.1, 1)"});
       $overviewRight.velocity({opacity: 1, translateY: "-30px"}, {visibility: "visible", delay: 475, duration: 500, easing: "cubic-bezier(0.6, 0.2, 0.1, 1)"});
     }

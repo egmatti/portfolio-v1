5525868a1faaa3054eae928fefae11589f076906
Emily Mattison <egmatti@gmail.com>
2017-08-06 14:06:16 -0400

    Fixed project desktop mockups section's margin bottom

--- js/dogs-booklet.js
+++ js/dogs-booklet.js
@@ -2,12 +2,16 @@
 // DOGS BOOKLET PAGE
 // ==========================================
 
-// DESKTOP MOCKUPS SECTION LAPTOP POSITIONING
+// DESKTOP MOCKUPS SECTION MARGIN AND LAPTOP POSITIONING
 
 $(document).ready(function(){
   var laptopHeight = $("#dogs-booklet-page .project-desktop-mockups-section__row--first").height();
   var laptopPosition = -(laptopHeight / 2);
   var rowMargin = (laptopHeight / 2);
+
+  var backgroundWhiteHeight = $("#dogs-booklet-page .project-desktop-mockups-section__background--white").outerHeight();
+  var backgroundGrayHeight = $("#dogs-booklet-page .project-desktop-mockups-section__background--gray").outerHeight();
+  var desktopMockupsSectionHeight = backgroundWhiteHeight + backgroundGrayHeight + rowMargin;
 
   if (mediaQuerySmall.matches) {
     // window width is less than 767px
@@ -17,6 +21,8 @@ $(document).ready(function(){
 
   } else {
     // window width is at least 1025px
+    $("#dogs-booklet-page .project-desktop-mockups-section").css({"margin-bottom": desktopMockupsSectionHeight});
+
     $("#dogs-booklet-page .project-desktop-mockups-section__row--first").css({"top": laptopPosition});
     $("#dogs-booklet-page .project-desktop-mockups-section__row--second").css({"margin-top": rowMargin});
   }

e1208f9dd69bf14ae2cab3432fab378fdbaf17ab
Emily Mattison <egmatti@gmail.com>
2017-08-06 16:47:07 -0400

    Added project section slider module

--- js/dogs-booklet.js
+++ js/dogs-booklet.js
@@ -13,6 +13,8 @@ $(document).ready(function(){
   var backgroundGrayHeight = $("#dogs-booklet-page .project-desktop-mockups-section__background--gray").outerHeight();
   var desktopMockupsSectionHeight = backgroundWhiteHeight + backgroundGrayHeight + rowMargin;
 
+  var borderHeight = backgroundWhiteHeight + backgroundGrayHeight;
+
   if (mediaQuerySmall.matches) {
     // window width is less than 767px
 
@@ -21,9 +23,21 @@ $(document).ready(function(){
 
   } else {
     // window width is at least 1025px
+    $("#dogs-booklet-page .project-desktop-mockups-section__border").css({"height": borderHeight});
     $("#dogs-booklet-page .project-desktop-mockups-section").css({"margin-bottom": desktopMockupsSectionHeight});
 
     $("#dogs-booklet-page .project-desktop-mockups-section__row--first").css({"top": laptopPosition});
     $("#dogs-booklet-page .project-desktop-mockups-section__row--second").css({"margin-top": rowMargin});
   }
+});
+
+
+// INSTAGRAM SLIDER
+
+$(".project-slider-section-for").slick({
+ arrows: true,
+ infinite: true,
+ slidesToScroll: 1,
+ slidesToShow: 3,
+ vertical: false
 });

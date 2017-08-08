884b521c2303855e59d168324bd5aa17346f3de0
Emily Mattison <egmatti@gmail.com>
2017-08-08 16:20:08 -0400

    Added content to and styled new Chronicle project page

--- js/chronicle.js
+++ js/chronicle.js
@@ -2,49 +2,20 @@
 // CHRONICLE PAGE
 // ==========================================
 
-// SCROLL REVEAL
-
-window.sr = ScrollReveal({distance: "30px", duration: 500, scale: 0});
-sr.reveal('.logo-section img', {delay: 250});
-
-sr.reveal('.colophon-section img', {delay: 250});
-
-sr.reveal('.collateral-section img', {delay: 250});
-
-
-// COLOPHON IMAGE CONTAINER HEIGHT
+// PROJECT MAIN IMAGE MARGIN
 
 $(document).ready(function(){
-  var colophonSectionDescriptionHeight = $("#chronicle-page .colophon-description").outerHeight();
+  var projectMainImageHeight = $("#chronicle-page .project-main-image-section__image-container").height();
+  var projectMainImageBackgroundMargin = (projectMainImageHeight / 2);
 
   if (mediaQuerySmall.matches) {
     // window width is less than 767px
-
+    $("#chronicle-page .project-main-image-section__background").css({"margin-bottom": projectMainImageBackgroundMargin});
   } else if (mediaQueryMedium.matches) {
     // window width is less than 1025px
-    $("#chronicle-page .colophon-image-container").css({"height": colophonSectionDescriptionHeight});
-  } else {
-    // window width is at least 767px
-    $("#chronicle-page .colophon-image-container").css({"height": colophonSectionDescriptionHeight});
-  }
-});
 
-
-// COLLATERAL SECTION HEIGHT
-
-$(document).ready(function(){
-  var collateralImageContainerHeight = $("#chronicle-page .collateral-image-container").outerHeight() + 150;
-  var collateralDescriptionTabletHeight = $("#chronicle-page .collateral-description").outerHeight() + 100;
-  var collateralDescriptionDesktopHeight = $("#chronicle-page .collateral-description").outerHeight() + 150;
-
-  if (mediaQuerySmall.matches) {
-    // window width is less than 767px
-
-  } else if (mediaQueryMedium.matches) {
-    // window width is less than 1025px
-    $("#chronicle-page .collateral-section").css({"height": collateralDescriptionDesktopHeight});
   } else {
     // window width is at least 1025px
-    $("#chronicle-page .collateral-section").css({"height": collateralDescriptionDesktopHeight});
+
   }
 });

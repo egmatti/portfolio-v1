bfef74cd8666086ccf1f099f15b753599494ab36
Emily Mattison <egmatti@gmail.com>
2017-08-10 17:08:35 -0400

    Changed content and styling on About page and deleted unneeded modules

--- js/about.js
+++ js/about.js
@@ -2,6 +2,25 @@
 // ABOUT PAGE
 // ==========================================
 
+// PROJECT MAIN IMAGE MARGIN
+
+$(document).ready(function(){
+  var projectMainImageHeight = $("#about-page .project-main-image-section__image-container").height();
+  var projectMainImageBackgroundMargin = (projectMainImageHeight / 2.5);
+
+  if (mediaQuerySmall.matches) {
+    // window width is less than 767px
+    $("#about-page .project-main-image-section__background").css({"margin-bottom": projectMainImageBackgroundMargin});
+  } else if (mediaQueryMedium.matches) {
+    // window width is less than 1025px
+
+  } else {
+    // window width is at least 1025px
+
+  }
+});
+
+
 // SCROLL REVEAL
 
 window.sr = ScrollReveal({distance: "30px", duration: 500, scale: 0});

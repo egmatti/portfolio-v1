2e48b67771dc6d3b9f89eaafdcf25d3ffe38007e
Emily Mattison <egmatti@gmail.com>
2017-08-11 18:10:39 -0400

    Started adding back in the Velocity overview animations

--- js/about.js
+++ js/about.js
@@ -2,6 +2,40 @@
 // ABOUT PAGE
 // ==========================================
 
+// Get the position from the top of the page
+var startY = 80;
+
+// Set overview div's displays to "none"
+var $overviewDescription = $(".project-overview-section__description");
+var $overviewImage = $(".project-overview-section img");
+
+$(window).on("scroll", function(){
+  checkY();
+});
+
+function checkY(){
+  if( $(window).scrollTop() > startY ){
+    $(window).off("scroll");
+    if (mediaQuerySmall.matches) {
+      // window width is less than 767px
+      $overviewDescription.velocity({translateY: "0px"});
+      $overviewImage.velocity({translateY: "0px"});
+    } else if (mediaQueryMedium.matches) {
+      // window width is less than 1025px
+      $overviewDescription.velocity({translateY: "0px"});
+      $overviewImage.velocity({translateY: "0px"});
+    } else {
+      // window width is at least 767px
+      $overviewDescription.velocity({opacity: 1, translateY: "-30px"}, {visibility: "visible", duration: 500, easing: "cubic-bezier(0.6, 0.2, 0.1, 1)"});
+      $overviewImage.velocity({opacity: 1, translateY: "-30px"}, {visibility: "visible", delay: 475, duration: 500, easing: "cubic-bezier(0.6, 0.2, 0.1, 1)"});
+    }
+  }
+};
+
+// Do this on load just in case the user starts half way down the page
+checkY();
+
+
 // PROJECT MAIN IMAGE MARGIN
 
 $(document).ready(function(){
